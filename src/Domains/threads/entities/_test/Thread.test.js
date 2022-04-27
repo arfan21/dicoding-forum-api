b65077e6c794212d8008acc2e01c41b5c7fe34ea
arfan21 <arfan.jadul10@gmail.com>
2022-04-27 22:18:14 +0700

    add unit test thread with comment & reply

--- src/Domains/threads/entities/_test/Thread.test.js
+++ src/Domains/threads/entities/_test/Thread.test.js
@@ -1,7 +1,8 @@
+const Comment = require('../../../comments/entities/Comment');
 const Thread = require('../Thread');
 
-describe('CreatedThread entities', () => {
-    it('should create createdThread object correctly', () => {
+describe('Thread entities', () => {
+    it('should create Thread object correctly', () => {
         // Arrange
         const payload = {
             id: 'id',
@@ -13,17 +14,90 @@ describe('CreatedThread entities', () => {
         };
 
         // Act
-        const createdThread = new Thread(payload);
+        const thread = new Thread(payload);
 
         // Assert
-        expect(createdThread.id).toEqual(payload.id);
-        expect(createdThread.title).toEqual(payload.title);
-        expect(createdThread.body).toEqual(payload.body);
-        expect(createdThread.date).toEqual(
-            payload.date.toISOString(),
+        expect(thread.id).toEqual(payload.id);
+        expect(thread.title).toEqual(payload.title);
+        expect(thread.body).toEqual(payload.body);
+        expect(thread.date).toEqual(payload.date.toISOString());
+        expect(thread.username).toEqual(payload.username);
+        expect(thread.comments).toEqual(payload.comments);
+    });
+
+    it('should create Thread object correctly with comments', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 'id',
+                    content: 'content',
+                    date: new Date(),
+                    username: 'username',
+                },
+            ],
+        };
+
+        // Act
+        const thread = new Thread(payload);
+
+        // Assert
+        expect(thread.id).toEqual(payload.id);
+        expect(thread.title).toEqual(payload.title);
+        expect(thread.body).toEqual(payload.body);
+        expect(thread.date).toEqual(payload.date.toISOString());
+        expect(thread.username).toEqual(payload.username);
+        expect(thread.comments[0]).toStrictEqual(
+            new Comment(payload.comments[0]),
         );
-        expect(createdThread.username).toEqual(payload.username);
-        expect(createdThread.comments).toEqual(payload.comments);
+    });
+
+    it('should create Thread object correctly with comments and replies', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 'id',
+                    content: 'content',
+                    date: new Date(),
+                    username: 'username',
+                    replies: [
+                        {
+                            id: 'id-reply',
+                            content: 'content-reply',
+                            date: new Date(),
+                            username: 'username-reply',
+                        },
+                    ],
+                },
+            ],
+        };
+
+        // Act
+        const thread = new Thread(payload);
+
+        // Assert
+        expect(thread.id).toEqual(payload.id);
+        expect(thread.title).toEqual(payload.title);
+        expect(thread.body).toEqual(payload.body);
+        expect(thread.date).toEqual(payload.date.toISOString());
+        expect(thread.username).toEqual(payload.username);
+        expect(thread.comments[0]).toStrictEqual(
+            new Comment(payload.comments[0]),
+        );
+        expect(thread.comments[0].replies[0]).toStrictEqual(
+            new Comment(payload.comments[0].replies[0]),
+        );
     });
 
     it('should throw error when payload not contain needed property', () => {
@@ -42,6 +116,60 @@ describe('CreatedThread entities', () => {
         );
     });
 
+    it('should throw error when payload comment not contain needed property', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 'id',
+                    content: 'content',
+                    date: new Date(),
+                },
+            ],
+        };
+
+        // Action & Assert
+        expect(() => new Thread(payload)).toThrowError(
+            'COMMENT.NOT_CONTAIN_NEEDED_PROPERTY',
+        );
+    });
+
+    it('should throw error when payload reply not contain needed property', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 'id',
+                    content: 'content',
+                    date: new Date(),
+                    username: 'username',
+                    replies: [
+                        {
+                            id: 'id-reply',
+                            content: 'content-reply',
+                            date: new Date(),
+                        },
+                    ],
+                },
+            ],
+        };
+
+        // Action & Assert
+        expect(() => new Thread(payload)).toThrowError(
+            'COMMENT.NOT_CONTAIN_NEEDED_PROPERTY',
+        );
+    });
+
     it('should throw error when payload not meet data type specification', () => {
         // Arrange
         const payload = {
@@ -56,6 +184,62 @@ describe('CreatedThread entities', () => {
         // Act & Assert
         expect(() => new Thread(payload)).toThrowError(
             'THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION',
+        );
+    });
+
+    it('should throw error when payload comment not meet data type specification', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 1,
+                    content: 1,
+                    date: 1,
+                    username: 1,
+                },
+            ],
+        };
+
+        // Act & Assert
+        expect(() => new Thread(payload)).toThrowError(
+            'COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION',
+        );
+    });
+
+    it('should throw error when payload reply not meet data type specification', () => {
+        // Arrange
+        const payload = {
+            id: 'id',
+            title: 'title',
+            body: 'body',
+            date: new Date(),
+            username: 'username',
+            comments: [
+                {
+                    id: 'id',
+                    content: 'content',
+                    date: new Date(),
+                    username: 'username',
+                    replies: [
+                        {
+                            id: 1,
+                            content: 1,
+                            date: 1,
+                            username: 1,
+                        },
+                    ],
+                },
+            ],
+        };
+
+        // Act & Assert
+        expect(() => new Thread(payload)).toThrowError(
+            'COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION',
         );
     });
 });
